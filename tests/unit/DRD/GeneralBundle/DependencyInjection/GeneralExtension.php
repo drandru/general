1f3185ec01a89943f08fc4ae30391803393ed586
SmirnovA <smirnovav10@mos.ru>
2019-10-10 17:38:18 +0300

    v4.3 tests

--- tests/unit/DRD/GeneralBundle/DependencyInjection/GeneralExtension.php
+++ tests/unit/DRD/GeneralBundle/DependencyInjection/GeneralExtension.php
@@ -4,6 +4,10 @@ namespace Tests\DRD\GeneralBundle\DependencyInjection;
 
 use DRD\GeneralBundle\DependencyInjection\GeneralExtension as BaseGeneralExtension;
 
+/**
+ * Class GeneralExtension
+ * @package Tests\DRD\GeneralBundle\DependencyInjection
+ */
 class GeneralExtension extends BaseGeneralExtension
 {
     const CONFIG_DIRECTORY = __DIR__ . '/test';
